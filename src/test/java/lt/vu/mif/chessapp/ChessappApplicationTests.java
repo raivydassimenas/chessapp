85d7d0b10088620ea7e5f06ea1fc3dbb00782a25
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-05-05 03:18:39 +0300

    Initial commit

---
+++ src/test/java/lt/vu/mif/chessapp/ChessappApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vu.mif.chessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
